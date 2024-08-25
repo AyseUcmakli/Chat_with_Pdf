397ca2279b0cda95c8328776adcd33107b4396cc
Aysucmkli <108420703+AyseUcmakli@users.noreply.github.com>
2024-08-25 21:41:52 +0300

    Update app.py

--- Langchain/app.py
+++ Langchain/app.py
@@ -10,6 +10,7 @@ from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 
+
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -17,6 +18,7 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
 
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
